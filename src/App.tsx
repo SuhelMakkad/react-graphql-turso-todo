f24436bba37acf5f2875b43669d4332f22bfa63e
Quarticdot AI <makadsuhel11@gmail.com>
2023-12-19 23:22:34 +0530

    added theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { ModeToggle } from '@/components/mode-toggle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div >
+      <ModeToggle />
+
+      <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,22 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+const getPath = (route: string) => path.resolve(__dirname, route);
+
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
-      "@/styles*": path.resolve(__dirname, "./src/common/styles*"),
-      "@/utils*": path.resolve(__dirname, "./src/common/utils*"),
-      "@/components*": path.resolve(__dirname, "./src/common/components*"),
-      "@": path.resolve(__dirname, "./src"),
+      "@/layouts": getPath("./src/layouts"),
+      "@/pages": getPath("./src/pages"),
+      "@/components": getPath("./src/common/components"),
+      "@/hooks": getPath("./src/common/hooks"),
+      "@/styles": getPath("./src/common/styles"),
+      "@/utils": getPath("./src/common/utils"),
+      "@/store": getPath("./src/common/store"),
+      "@/assets": getPath("./src/assets"),
+      "@/": getPath("./src"),
+      "@": getPath("./src"),
     },
   },
+  plugins: [react()],
 })
