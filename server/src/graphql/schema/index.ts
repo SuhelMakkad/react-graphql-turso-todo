05c201e7e0016f997f297ceb37e596bcdddeff1f
Quarticdot AI <makadsuhel11@gmail.com>
2023-12-23 15:34:11 +0530

    mu

--- server/src/graphql/schema/index.ts
+++ server/src/graphql/schema/index.ts
@@ -1,8 +1,8 @@
 import { readFileSync } from "fs";
 
-let filepath = "./src/graphql/schema/schema.graphql";
+let filepath = "./graphql/schema/schema.graphql";
 if (process.env.ENV === "dev") {
-  filepath = "./graphql/schema/schema.graphql";
+  filepath = "./src/graphql/schema/schema.graphql";
 }
 
 export const typeDefs = readFileSync(filepath, {
