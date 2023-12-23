d9276dafad016c70fbc00ddab5d79486fcfc3c41
Quarticdot AI <makadsuhel11@gmail.com>
2023-12-23 16:40:58 +0530

    added grahql hosted url for client

--- client/src/common/graphql/index.tsx
+++ client/src/common/graphql/index.tsx
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-const graphqlUrl = "http://localhost:8000/graphql";
+const graphqlUrl = import.meta.env.PROD
+  ? "http://localhost:8000/graphql"
+  : "https://react-graphql-turso-todo.vercel.app/graphql";
 
 export const apolloClient = new ApolloClient({
   uri: graphqlUrl,
