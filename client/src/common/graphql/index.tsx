81b966575366e81e4b9e6fb969539320ee4489c8
Quarticdot AI <makadsuhel11@gmail.com>
2023-12-24 12:51:59 +0530

    updated graphql prod url

--- client/src/common/graphql/index.tsx
+++ client/src/common/graphql/index.tsx
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const graphqlUrl = import.meta.env.PROD
-  ? "http://localhost:8000/graphql"
-  : "https://react-graphql-turso-todo.vercel.app/graphql";
+  ? "https://react-graphql-turso-todo.vercel.app/graphql"
+  : "http://localhost:8000/graphql";
 
 export const apolloClient = new ApolloClient({
   uri: graphqlUrl,
