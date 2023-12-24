9dbf0b151720c6eb0e19971d761f4988aa1484d9
Quarticdot AI <makadsuhel11@gmail.com>
2023-12-24 12:46:27 +0530

    whitespace changes

--- client/codegen.ts
+++ client/codegen.ts
@@ -3,7 +3,7 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   schema: "../server/src/graphql/schema/schema.graphql",
   documents: ["src/**/!(*.d).{ts,tsx}", "src/**/*/!(*.d).{ts,tsx}"],
-  ignoreNoDocuments: true, // for better experience with the watcher
+  ignoreNoDocuments: true,
   generates: {
     "./src/common/graphql/types/": {
       preset: "client",
