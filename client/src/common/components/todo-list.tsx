0fbc77e2f6e1f16391d9d0aa28dd147b48ca12ac
Quarticdot AI <makadsuhel11@gmail.com>
2023-12-24 14:53:31 +0530

    added empty state

--- client/src/common/components/todo-list.tsx
+++ client/src/common/components/todo-list.tsx
@@ -9,6 +9,15 @@ const loadingState = (
   </div>
 );
 
+const emptyState = (
+  <div className="flex text-muted-foreground h-[30vh] justify-center flex-col gap-2 items-center">
+    <div className="text-center">
+      <span className="text-xl font-medium">All Set!</span>
+      <p className="italic">No pending item</p>
+    </div>
+  </div>
+);
+
 const TodoList = () => {
   const { data, loading } = useTodosQuery();
 
@@ -16,9 +25,13 @@ const TodoList = () => {
     return loadingState;
   }
 
+  if (!data?.todos?.length) {
+    return emptyState;
+  }
+
   return (
     <div className="flex flex-col gap-2">
-      {data?.todos?.map((todo) => todo && <TodoCard key={todo.id} {...todo} />)}
+      {data.todos.map((todo) => todo && <TodoCard key={todo.id} {...todo} />)}
     </div>
   );
 };
