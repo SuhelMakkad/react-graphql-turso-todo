024cee55858872a40762a3c907418fab6b7096f9
Quarticdot AI <makadsuhel11@gmail.com>
2023-12-24 12:50:00 +0530

    whitespace changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
-import { ApolloGraphqlProvider } from "./common/graphql/index.tsx";
+import App from "./App";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ApolloGraphqlProvider } from "@/graphql/index";
 
-import "./common/styles/global.css";
+import "@/styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
